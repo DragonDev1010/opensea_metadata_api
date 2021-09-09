7f8604427df4e106d30c360e73ea1d10d1602585
dragonyu1010 <provic0099@gmail.com>
2021-09-08 19:56:42 -0700

    change metadata json

--- src/database.js
+++ src/database.js
@@ -6,24 +6,11 @@ module.exports = {
     "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
     "name": "My NFT 0"
   },
-  // "1": {
-  //   "name": "Chunky Monkey",
-  //   "birthday": "10-18"
-  // },
-  // "2": {
-  //   "name": "Flerp Derp",
-  //   "birthday": "01-08"
-  // },
-  // "3": {
-  //   "name": "Buttle McFuddle",
-  //   "birthday": "01-22"
-  // },
-  // "4": {
-  //   "name": "Quiero Pancakes",
-  //   "birthday": "03-04"
-  // },
-  // "5": {
-  //   "name": "Guino the Penguin",
-  //   "birthday": "03-09"
-  // }
+  "0": {
+    "id": 1,
+    "description": "This NFT is created by David",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://ipfsgateway.makersplace.com/ipfs/QmQ2NkJW8auAHKBhw18tjNzZV7P8p379zoSDJeHPdTJ9ej",
+    "name": "Chunky Test"
+  },
 }
